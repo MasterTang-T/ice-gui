40c730ae53c948f3b03dbe50e9b007b4c98989e0
Tang <1286545749@qq.com>
2020-09-07 11:30:42 +0800

    add help page

--- index.js
+++ index.js
@@ -26,75 +26,25 @@ function createWindow() {
 }
 /**
  * 注册键盘快捷键
- * 其中：label: '切换开发者工具',这个可以在发布时注释掉
  */
-let template = [{
-        label: 'Edit ( 操作 )',
-        submenu: [{
-            label: 'Copy ( 复制 )',
-            accelerator: 'CmdOrCtrl+C',
-            role: 'copy'
-        }, {
-            label: 'Paste ( 粘贴 )',
-            accelerator: 'CmdOrCtrl+V',
-            role: 'paste'
-        }, {
-            label: 'Reload ( 重新加载 )',
-            accelerator: 'CmdOrCtrl+R',
-            click: function (item, focusedWindow) {
-                if (focusedWindow) {
-                    // on reload, start fresh and close any old
-                    // open secondary windows
-                    if (focusedWindow.id === 1) {
-                        BrowserWindow.getAllWindows().forEach(function (win) {
-                            if (win.id > 1) {
-                                win.close()
-                            }
-                        })
-                    }
-                    focusedWindow.reload()
-                }
+let template = [
+    {
+        label: '首页',
+        role: 'home',
+        click: function (item,focusedWindow) {
+            if (focusedWindow) {
+                focusedWindow.loadFile('index.html')
             }
-        }]
+        }
     },
     {
-        label: 'Window ( 窗口 )',
-        role: 'window',
-        submenu: [{
-            label: 'Minimize ( 最小化 )',
-            accelerator: 'CmdOrCtrl+M',
-            role: 'minimize'
-        }, {
-            label: 'Close ( 关闭 )',
-            accelerator: 'CmdOrCtrl+W',
-            role: 'close'
-        }, {
-            label: '切换开发者工具',
-            accelerator: (function () {
-                if (process.platform === 'darwin') {
-                    return 'Alt+Command+I'
-                } else {
-                    return 'Ctrl+Shift+I'
-                }
-            })(),
-            click: function (item, focusedWindow) {
-                if (focusedWindow) {
-                    focusedWindow.toggleDevTools()
-                }
-            }
-        }, {
-            type: 'separator'
-        }]
-    },
-    {
-        label: 'Help ( 帮助 ) ',
+        label: '说明',
         role: 'help',
-        submenu: [{
-            label: 'FeedBack ( 意见反馈 )',
-            click: function () {
-                console.log(1111)
+        click: function (item,focusedWindow) {
+            if (focusedWindow) {
+                focusedWindow.loadFile('help.html')
             }
-        }]
+        },
     }
 ]
 
